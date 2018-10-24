7a3462e9082dd95f0e8d0ae072bfebbe1eaf987f
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2018-10-24 12:38:55 +0100

    Added auth

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const express       = require('express'),
       passport      = require('passport'),
       flash         = require('connect-flash'),
       session       = require('express-session'),
+      mysql           = require('mysql'),
       dotenv        = require('dotenv').config(),
       app           = express();
 //Setiing up database using mongoDB ORM
-mongoose.connect(process.env.mlab_url,{useNewUrlParser: true});
+// mongoose.connect(process.env.mlab_url,{useNewUrlParser: true});
 
 require('./config/passport')(passport);
 
@@ -25,6 +26,29 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
+// Connecting to mysql database
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.sql_user,
+  password: process.env.sql_pass
+});
+
+con.connect((err) => {
+  if (err) {
+    console.log(process.env.sql_user);
+    console.log(process.env.sql_pass);
+    console.log('Failed to connect',err);
+  } else {
+    console.log('Connected successfully');
+    con.query('create database clinicManagement',(err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Database successfully created');
+      }
+    });
+  }
+});
 app.use((req, res, next) => {
   res.locals.currentUser = req.session.user;
   next();
